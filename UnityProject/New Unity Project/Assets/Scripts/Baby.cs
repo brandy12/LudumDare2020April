5923feca9f13204376ddcd092dcc7e59fb56633a
brandongonzalez <brand_1995@hotmail.com>
2020-04-18 13:45:22 +0200

    Prefab Babies with first input and bars implementation

--- UnityProject/New Unity Project/Assets/Scripts/Baby.cs
+++ UnityProject/New Unity Project/Assets/Scripts/Baby.cs
@@ -4,15 +4,99 @@ using UnityEngine;
 
 public class Baby : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    public int id { get; private set; }
+
+    //*********************************************************
+    //          BARS VARIABLES
+    //*********************************************************
+
+    //bars from 0 to 100. 0 means completely unsatisfied, 100 means totally satisfied
+    float food;
+    float entertainment;
+    float diaper;
+
+    //amount of bars decreasing by second
+    float speed_food;
+    float speed_entertainment;
+    float speed_diaper;
+
+    //*********************************************************
+    //          UNITY FUNCTIONS
+    //*********************************************************
+
+    void Start() {
+        Initialize();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Initialize() {
+        // initialize variables for the beginning of the party
+        // (to be called when trying to replay)
+
+        id = -1; // -1 means undefined id
+
+        food = 100;
+        entertainment = 100;
+        diaper = 100;
+
+        speed_food = 1.0f;
+        speed_entertainment = 1.0f;
+        speed_diaper = 1.0f;
+
+    }
+
+    void Update() {
+        BarsManagement();
+    }
+
+
+    //*********************************************************
+    //          ID
+    //*********************************************************
+
+    public void SetId(int _id) {
+        id = _id;
+    }
+
+    //*********************************************************
+    //          BARS FUNCTIONS
+    //*********************************************************
+
+    void BarsManagement() {
+        if(food>0)
+            food -= speed_food * Time.deltaTime;
+        if (food < 0)
+            food = 0;
+            
+        if (entertainment > 0)
+            entertainment -= speed_entertainment * Time.deltaTime;
+        if (entertainment < 0)
+            entertainment = 0;
+
+        if (diaper > 0)
+            diaper -= speed_diaper * Time.deltaTime;
+        if (diaper < 0)
+            diaper = 0;
+    }
+
+    bool critical_need() {
+        return (food <= 0 || entertainment <= 0 || diaper <= 0) ;
+    }
+
+
+    //*********************************************************
+    //          ACTIONS FUNCTIONS
+    //*********************************************************
+
+    public void ChangeDiaper() {
+        diaper = 100;
+    }
+
+    public void GiveFood() {
+        food = 100;
+    }
+
+    public void Entertain() {
+        entertainment = 100;
     }
 }
--- UnityProject/New Unity Project/Assets/Scripts/MainGame.cs
+++ UnityProject/New Unity Project/Assets/Scripts/MainGame.cs
@@ -4,15 +4,115 @@ using UnityEngine;
 
 public class MainGame : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
+    //*********************************************************
+    //          BABY VARIABLES
+    //*********************************************************
+
+    int number_babies;
+    int id_selected_baby;
+
+    [SerializeField] Transform Babies;
+    [SerializeField] Baby baby_prefab;
+
+
+    //*********************************************************
+    //          ACTIONS VARIABLES
+    //*********************************************************
+    
+
+
+    //*********************************************************
+    //          UNITY FUNCTIONS
+    //*********************************************************
+
     void Start()
     {
-        
+        Initialize();
     }
 
-    // Update is called once per frame
+    public void Initialize() {
+        // initialize variables for the beginning of the party
+        // (to be called when trying to replay)
+
+        number_babies = 1;
+        id_selected_baby = 1;
+
+        BabiesGeneration();
+
+    }
+    
     void Update()
     {
-        
+        InputsManagement();
+    }
+
+
+
+    //*********************************************************
+    //          INPUTS FUNCTIONS
+    //*********************************************************
+
+    void InputsManagement() {
+
+        //baby selection
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            id_selected_baby -= 1;
+            if (id_selected_baby < 0)
+                id_selected_baby = number_babies - 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            id_selected_baby += 1;
+            if (id_selected_baby >= number_babies)
+                id_selected_baby = 0;
+        }
+
+        //actions
+        if (selected_baby != null) {
+            if (Input.GetKeyDown(KeyCode.Alpha1)) {
+                selected_baby.GiveFood();
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2)) {
+                selected_baby.ChangeDiaper();
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3)) {
+                selected_baby.Entertain();
+            }
+        }
+
+    }
+
+
+    //*********************************************************
+    //          BABIES GENERATION
+    //*********************************************************
+
+    void BabiesGeneration() {
+        for (int i = 0; i < number_babies; ++i) {
+            Baby b = Baby.Instantiate(baby_prefab);
+
+            b.transform.SetParent(Babies, false);
+            b.SetId(i);
+            b.transform.position = Vector3.zero;
+
+        }
+    }
+
+
+    //*********************************************************
+    //          BABY SELECTION
+    //*********************************************************
+
+    public Baby selected_baby {
+        get {
+            foreach (Transform t in Babies) {
+                if (t.GetComponent<Baby>().id == id_selected_baby)
+                    return t.GetComponent<Baby>();
+            }
+            return null;
+        }
     }
 }
